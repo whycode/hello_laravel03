f09324999a605dd69b42081060a966e61087ef54
Varid <3407135351@qq.com>
2020-08-16 17:44:51 +0800

    完成用户注册功能

--- config/app.php
+++ config/app.php
@@ -80,8 +80,8 @@ return [
     |
     */
 
-    'locale' => 'en',
-
+//    'locale' => 'en',
+    'locale' => 'zh-CN',
     /*
     |--------------------------------------------------------------------------
     | Application Fallback Locale
@@ -158,7 +158,8 @@ return [
         Illuminate\Redis\RedisServiceProvider::class,
         Illuminate\Auth\Passwords\PasswordResetServiceProvider::class,
         Illuminate\Session\SessionServiceProvider::class,
-        Illuminate\Translation\TranslationServiceProvider::class,
+//        Illuminate\Translation\TranslationServiceProvider::class,
+        Overtrue\LaravelLang\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
