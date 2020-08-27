98816014ac06f8d0fc4a31a408aaca3e457939a5
Varid <3407135351@qq.com>
2020-08-27 21:39:00 +0800

    微博数据模型

--- app/Models/User.php
+++ app/Models/User.php
@@ -43,7 +43,10 @@ class User extends Authenticatable
         return "http://www.gravatar.com/avatar/$hash?s=$size";
     }
 
-
+    public function statuses()
+    {
+        return $this->hasMany(Status::class);
+    }
 
 
 
