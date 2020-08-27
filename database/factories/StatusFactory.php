a278d2de9c038e41d34b6fb8bfbd2b3af2ee0677
Varid <3407135351@qq.com>
2020-08-27 22:57:11 +0800

    用户微博列表

---
+++ database/factories/StatusFactory.php
@@ -0,0 +1,17 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model;
+use Faker\Generator as Faker;
+
+$factory->define(App\Models\Status::class, function (Faker $faker) {
+    $date_time = $faker->date .' ' . $faker->time;
+    return [
+        'user_id' => $faker->randomElement(['1','2','3']),
+        'content' => $faker->text(),
+        'created_at' => $date_time,
+        'updated_at' => $date_time,
+    ];
+});
+
