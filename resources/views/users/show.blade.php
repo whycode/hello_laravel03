0d6f2b8fd8ccfe75f0025cb99f43ffdb6e24eb8f
Varid <3407135351@qq.com>
2020-08-30 11:46:05 +0800

    社交统计信息

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -4,10 +4,13 @@
 @section('content')
     <div class="row">
         <div class="offset-md-2 col-md-8">
-
             <section class="user_info">
                 @include('shared._user_info',['user'=>$user])
             </section>
+            <section class="stats mt-2">
+                @include('shared._stats',['user'=>$user])
+            </section>
+            <hr>
             <section class="status">
                 @if($statuses->count()>0)
                     <ul class="list-unstyled">
@@ -22,7 +25,6 @@
                     <p>没有数据</p>
                 @endif
             </section>
-
         </div>
     </div>
 @stop
